79ce675923662f6eaf36a75325b1f22b3360f4a8
Tomas Reimers <tomasreimers@gmail.com>
2021-07-15 17:11:16 -0400

    Updates (#14)

--- src/commands/log/index.ts
+++ src/commands/log/index.ts
@@ -7,9 +7,13 @@ type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 export default class LogCommand extends AbstractCommand<typeof args> {
   static args = args;
   public async _execute(argv: argsT) {
-    execSync(
-      `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all`,
-      { stdio: "inherit" }
-    );
+    try {
+      execSync(
+        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all`,
+        { stdio: "inherit" }
+      );
+    } catch (e) {
+      // Ignore errors (this just means they quit git log)
+    }
   }
 }
