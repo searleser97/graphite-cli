d6f65cecddee8df2f0380d003b3d0128951835dc
Greg Foster <gregorymfoster@gmail.com>
2021-07-22 11:31:03 -0400

    [Log] Update colors in the log command output

--- src/commands/log/index.ts
+++ src/commands/log/index.ts
@@ -9,7 +9,7 @@ export default class LogCommand extends AbstractCommand<typeof args> {
   public async _execute(argv: argsT): Promise<void> {
     try {
       execSync(
-        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all`,
+        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all`,
         { stdio: "inherit" }
       );
     } catch (e) {
