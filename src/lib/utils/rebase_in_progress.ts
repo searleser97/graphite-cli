d24b10085c8aadfa4ccf49a5ab958987783ba806
Greg Foster <gregorymfoster@users.noreply.github.com>
2021-07-24 11:37:04 -0400

    [Util] Detect if rebase is in progress (#48)

---
+++ src/lib/utils/rebase_in_progress.ts
@@ -0,0 +1,15 @@
+import { execSync } from "child_process";
+import fs from "fs-extra";
+import path from "path";
+export function rebaseInProgress(opts?: { dir: string }): boolean {
+  let rebaseDir = path.join(
+    execSync(`git ${opts ? `-C ${opts.dir}` : ""} rev-parse --git-dir`)
+      .toString()
+      .trim(),
+    "rebase-merge"
+  );
+  if (opts) {
+    rebaseDir = path.join(opts.dir, rebaseDir);
+  }
+  return fs.existsSync(rebaseDir);
+}
