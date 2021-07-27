0721f220fbc61be83a400cc1cf05ee4fafcb2eda
Greg Foster <gregorymfoster@gmail.com>
2021-07-26 22:38:49 -0400

    fix(subcommands): throw yargs warning if wrong subcommand

--- src/commands/branch.ts
+++ src/commands/branch.ts
@@ -3,7 +3,10 @@ import { Argv } from "yargs";
 export const command = "branch <command>";
 export const desc = "Branch commands";
 export const builder = function (yargs: Argv): Argv {
-  return yargs.commandDir("branch-commands", {
-    extensions: ["js"],
-  });
+  return yargs
+    .commandDir("branch-commands", {
+      extensions: ["js"],
+    })
+    .strict()
+    .demandCommand();
 };
--- src/commands/stack.ts
+++ src/commands/stack.ts
@@ -3,7 +3,10 @@ import yargs from "yargs";
 export const command = "stack <command>";
 export const desc = "Stack commands";
 export const builder = function (yargs: yargs.Argv): yargs.Argv {
-  return yargs.commandDir("stack-commands", {
-    extensions: ["js"],
-  });
+  return yargs
+    .commandDir("stack-commands", {
+      extensions: ["js"],
+    })
+    .strict()
+    .demandCommand();
 };
