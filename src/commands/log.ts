e48d86479c9da81a8c9201f76152c1295e57d5b9
Greg Foster <gregorymfoster@gmail.com>
2021-07-27 10:44:55 -0400

    refactor(log): move log command to new style

---
+++ src/commands/log.ts
@@ -0,0 +1,21 @@
+import { execSync } from "child_process";
+import { profiledHandler } from "../lib/telemetry";
+
+const args = {} as const;
+
+export const command = "log";
+export const description = "Log stack";
+export const builder = args;
+
+export const handler = async (): Promise<void> => {
+  return profiledHandler(command, async () => {
+    try {
+      execSync(
+        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all`,
+        { stdio: "inherit" }
+      );
+    } catch (e) {
+      // Ignore errors (this just means they quit git log)
+    }
+  });
+};
