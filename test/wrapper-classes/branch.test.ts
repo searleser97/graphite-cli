d2e831d655341cd3cf0e9b4785f20b4d622827c3
Greg Foster <gregorymfoster@gmail.com>
2021-07-30 13:12:45 -0400

    ci(branch_wrapper): refactor to use scenes

--- test/wrapper-classes/branch.test.ts
+++ test/wrapper-classes/branch.test.ts
@@ -1,62 +1,41 @@
 import { expect } from "chai";
-import fs from "fs-extra";
-import tmp from "tmp";
 import Branch from "../../src/wrapper-classes/branch";
-import { execCliCommand } from "../utils/exec_cli_command";
-import GitRepo from "../utils/git_repo";
+import { allScenes } from "../scenes";
+import { configureTest } from "../utils";
 
-describe("Branch tests", function () {
-  let tmpDir: tmp.DirResult;
-  let repo: GitRepo;
-  const oldDir = __dirname;
-  this.beforeEach(() => {
-    tmpDir = tmp.dirSync();
-    process.chdir(tmpDir.name);
-    repo = new GitRepo(tmpDir.name);
-    repo.createChangeAndCommit("1", "first");
+for (const scene of allScenes) {
+  describe("Branch tests", function () {
+    configureTest(this, scene);
+
+    it("Can list git parent for a branch", () => {
+      scene.repo.createChange("2");
+      scene.repo.execCliCommand(`branch create "a" -s`);
+
+      const branch = new Branch("a");
+      expect(branch.getParentsFromGit()[0].name).to.equal("main");
+    });
+
+    it("Can list parent based on meta for a branch", () => {
+      scene.repo.createChange("2");
+      scene.repo.execCliCommand(`branch create "a" -s`);
+
+      const branch = new Branch("a");
+      expect(branch.getParentFromMeta()).is.not.undefined;
+      expect(branch.getParentFromMeta()?.name).to.equal("main");
+    });
+
+    it("Can fetch branches that point to the same commit", () => {
+      scene.repo.createAndCheckoutBranch("a");
+      scene.repo.createChangeAndCommit("2");
+      scene.repo.createAndCheckoutBranch("b");
+      scene.repo.createAndCheckoutBranch("c");
+      expect(
+        new Branch("a")
+          .branchesWithSameCommit()
+          .map((b) => b.name)
+          .sort()
+          .join(", ")
+      ).to.eq("b, c");
+    });
   });
-  afterEach(() => {
-    process.chdir(oldDir);
-    fs.emptyDirSync(tmpDir.name);
-    tmpDir.removeCallback();
-  });
-
-  it("Can list git parent for a branch", () => {
-    repo.createChange("2");
-    execCliCommand(`branch create "a" -s`, { fromDir: tmpDir.name });
-
-    const branch = new Branch("a");
-    expect(branch.getParentsFromGit()[0].name).to.equal("main");
-
-    process.chdir(oldDir);
-    fs.emptyDirSync(tmpDir.name);
-    tmpDir.removeCallback();
-  });
-
-  it("Can list parent based on meta for a branch", () => {
-    repo.createChange("2");
-    execCliCommand(`branch create "a" -s`, { fromDir: tmpDir.name });
-
-    const branch = new Branch("a");
-    expect(branch.getParentFromMeta()).is.not.undefined;
-    expect(branch.getParentFromMeta()!.name).to.equal("main");
-
-    process.chdir(oldDir);
-    fs.emptyDirSync(tmpDir.name);
-    tmpDir.removeCallback();
-  });
-
-  it("Can fetch branches that point to the same commit", () => {
-    repo.createAndCheckoutBranch("a");
-    repo.createChangeAndCommit("2");
-    repo.createAndCheckoutBranch("b");
-    repo.createAndCheckoutBranch("c");
-    expect(
-      new Branch("a")
-        .branchesWithSameCommit()
-        .map((b) => b.name)
-        .sort()
-        .join(", ")
-    ).to.eq("b, c");
-  });
-});
+}
