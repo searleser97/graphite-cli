6a73988b3b5c64ebd5d8bb275c6d17a0d71a1e34
Greg Foster <gregorymfoster@gmail.com>
2021-08-02 16:12:23 -0400

    chore(bump): v0.6.1

---
+++ test/utils/expect_commits.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { GitRepo } from "../../src/lib/utils";
+export function expectCommits(repo: GitRepo, commitMessages: string): void {
+  expect(
+    repo
+      .listCurrentBranchCommitMessages()
+      .slice(0, commitMessages.split(",").length)
+      .join(", ")
+  ).to.equal(commitMessages);
+}
