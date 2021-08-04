c4d691569137a461b2f58bc4cd8f4293b928520d
Nicholas Yan <nicholasyan11@gmail.com>
2021-08-04 12:43:17 -0400

    [Log] Initial MVP (#123)
    * [Log] Initial MVP
    
    * update highlighting and trailing branches color
    
    Co-authored-by: Greg Foster <gregorymfoster@users.noreply.github.com>

---
+++ src/actions/print_stack.ts
@@ -0,0 +1,80 @@
+import chalk from "chalk";
+import { getTrunk } from "../lib/utils/trunk";
+import Branch from "../wrapper-classes/branch";
+
+type TPrintStackConfig = {
+  currentBranch: Branch | null;
+  offTrunk: boolean;
+};
+
+export function printStack(
+  branch: Branch,
+  indentLevel: number,
+  config: TPrintStackConfig
+): void {
+  const children = branch.getChildrenFromGit();
+  const currPrefix = getPrefix(indentLevel, config);
+
+  children.forEach((child, i) => {
+    printStack(child, indentLevel + i, config);
+  });
+
+  // 1) if there is only 1 child, we only need to continue the parent's stem
+  // 2) if there are multiple children, the 2..n children branch off
+  //    horizontally
+  const numChildren = children.length;
+  if (numChildren > 1) {
+    let newBranchOffshoots = "│";
+    // we only need to draw numChildren - 1 offshots since the first child
+    // continues the parent's main stem
+    for (let i = 1; i < numChildren; i++) {
+      if (i < numChildren - 1) {
+        newBranchOffshoots += "──┴";
+      } else {
+        newBranchOffshoots += "──┘";
+      }
+    }
+    console.log(currPrefix + newBranchOffshoots);
+    console.log(currPrefix + "│");
+  }
+
+  // print lines of branch info
+  const branchInfo = getBranchInfo(branch, config);
+  branchInfo.forEach((line) => console.log(currPrefix + line));
+
+  // print trailing stem
+  // note: stem directly behind trunk should be dotted
+  console.log(
+    currPrefix +
+      (!config.offTrunk && branch.name === getTrunk().name ? "․" : "│")
+  );
+}
+
+function getPrefix(indentLevel: number, config: TPrintStackConfig): string {
+  let prefix = "";
+  for (let i = 0; i < indentLevel; i++) {
+    // if we're behind trunk, the stem of trunk's branch should be dotted
+    if (i === 0) {
+      prefix += config.offTrunk ? "│  " : "․  ";
+    } else {
+      prefix += "│  ";
+    }
+  }
+  return prefix;
+}
+
+function getBranchInfo(branch: Branch, config: TPrintStackConfig): string[] {
+  const branchInfo = [];
+
+  const isCurrentBranch = config.currentBranch?.name === branch.name;
+  const prInfo = branch.getPRInfo();
+
+  const dot = isCurrentBranch ? chalk.cyan("◉") : "◯";
+  const branchName = isCurrentBranch
+    ? chalk.cyan(`${branch.name} (current)`)
+    : chalk.blueBright(branch.name);
+  const pr = prInfo !== undefined ? chalk.yellow(`PR #${prInfo.number}`) : "";
+  branchInfo.push(`${dot} ${branchName} ${pr}`);
+
+  return branchInfo;
+}
--- src/commands/log.ts
+++ src/commands/log.ts
@@ -1,6 +1,10 @@
+import chalk from "chalk";
+import { printStack } from "../actions/print_stack";
+import { profile } from "../lib/telemetry";
+import { getTrunk } from "../lib/utils/trunk";
+import Branch from "../wrapper-classes/branch";
 import { execSync } from "child_process";
 import yargs from "yargs";
-import { profile } from "../lib/telemetry";
 
 const args = {} as const;
 
@@ -13,12 +17,46 @@ type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 export const handler = async (argv: argsT): Promise<void> => {
   return profile(argv, async () => {
     try {
-      execSync(
-        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all`,
-        { stdio: "inherit" }
-      );
+      printTrunkLog();
+      await printStacksBehindTrunk();
     } catch (e) {
       // Ignore errors (this just means they quit git log)
     }
   });
 };
+
+function printTrunkLog(): void {
+  const trunk = getTrunk();
+  printStack(trunk, 0, {
+    currentBranch: Branch.getCurrentBranch(),
+    offTrunk: true,
+  });
+}
+
+async function printStacksBehindTrunk(): Promise<void> {
+  const trunk = getTrunk();
+  const branchesWithoutParents = (
+    await Branch.getAllBranchesWithoutParents()
+  ).filter((branch) => branch.name !== trunk.name);
+  if (branchesWithoutParents.length === 0) {
+    return;
+  }
+
+  console.log("․");
+  console.log("․");
+  console.log(`․  ${chalk.bold(`Stack(s) below trail ${trunk.name}.`)}`);
+  console.log(
+    `․  To fix a stack, check out the stack and run \`gp stack fix\`.`
+  );
+  console.log("․");
+
+  branchesWithoutParents.forEach((branch) => {
+    console.log("․");
+    printStack(branch, 1, {
+      currentBranch: Branch.getCurrentBranch(),
+      offTrunk: false,
+    });
+    console.log(`◌──┘`);
+    console.log("․");
+  });
+}
