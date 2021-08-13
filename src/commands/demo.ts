cb0567e5e950e90a46cdb1d4cdcad5e9d7d82949
Tomas Reimers <tomasreimers@gmail.com>
2021-08-13 10:30:09 -0400

    Submit in demo (#190)

--- src/commands/demo.ts
+++ src/commands/demo.ts
@@ -2,7 +2,7 @@ import { execSync } from "child_process";
 import tmp from "tmp";
 import yargs from "yargs";
 import { profile } from "../lib/telemetry";
-import { GitRepo } from "../lib/utils";
+import { GitRepo, makeId } from "../lib/utils";
 
 export const command = "demo";
 export const description = false;
@@ -17,24 +17,26 @@ export const handler = async (argv: argsT): Promise<void> => {
     console.log(tmpDir.name);
     const repo = new GitRepo(tmpDir.name);
 
+    const id = makeId(4);
+
     repo.createChangeAndCommit("First commit");
     repo.createChangeAndCommit("Second commit");
 
     repo.createChange("[Product] Add review queue filter api");
     execCliCommand(
-      "branch create 'tr--review_queue_api' -m '[Product] Add review queue filter api'",
+      `branch create 'tr-${id}--review_queue_api' -m '[Product] Add review queue filter api'`,
       { fromDir: tmpDir.name }
     );
 
     repo.createChange("[Product] Add review queue filter server");
     execCliCommand(
-      "branch create 'tr--review_queue_server' -m '[Product] Add review queue filter server'",
+      `branch create 'tr-${id}--review_queue_server' -m '[Product] Add review queue filter server'`,
       { fromDir: tmpDir.name }
     );
 
     repo.createChange("[Product] Add review queue filter frontend");
     execCliCommand(
-      "branch create 'tr--review_queue_frontend' -m '[Product] Add review queue filter frontend'",
+      `branch create 'tr-${id}--review_queue_frontend' -m '[Product] Add review queue filter frontend'`,
       { fromDir: tmpDir.name }
     );
 
@@ -42,7 +44,7 @@ export const handler = async (argv: argsT): Promise<void> => {
 
     repo.createChange("[Bug Fix] Fix crashes on reload");
     execCliCommand(
-      "branch create 'tr--fix_crash_on_reload' -m '[Bug Fix] Fix crashes on reload'",
+      `branch create 'tr-${id}--fix_crash_on_reload' -m '[Bug Fix] Fix crashes on reload'`,
       { fromDir: tmpDir.name }
     );
 
@@ -50,11 +52,17 @@ export const handler = async (argv: argsT): Promise<void> => {
 
     repo.createChange("[Bug Fix] Account for empty state");
     execCliCommand(
-      "branch create 'tr--account_for_empty_state' -m '[Bug Fix] Account for empty state'",
+      `branch create 'tr-${id}--account_for_empty_state' -m '[Bug Fix] Account for empty state'`,
       { fromDir: tmpDir.name }
     );
 
     repo.checkoutBranch("main");
+
+    execSync(
+      "git remote add origin git@github.com:screenplaydev/graphite-demo-repo.git",
+      { cwd: tmpDir.name }
+    );
+    execSync("git push origin main -f", { cwd: tmpDir.name });
   });
 };
 
