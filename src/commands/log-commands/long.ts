b6271ec8eca38fa213c9e24a9267bab28cd4bf4c
Nicholas Yan <nicholasyan11@gmail.com>
2021-08-17 14:40:06 -0500

    [Log] Hide remote branches in log -c (#217)

--- src/commands/log-commands/long.ts
+++ src/commands/log-commands/long.ts
@@ -15,7 +15,7 @@ export const handler = async (argv: argsT): Promise<void> => {
     // If this flag is passed, print the old logging style:
     try {
       execSync(
-        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all`,
+        `git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --branches`,
         { stdio: "inherit" }
       );
     } catch (e) {
