a6ce69489f8981f942de0e3a7bea02366f9b990e
Greg Foster <gregorymfoster@users.noreply.github.com>
2021-08-17 09:04:10 -0400

    chore(copy): adjust fix wording (#207)

--- src/actions/fix.ts
+++ src/actions/fix.ts
@@ -31,19 +31,21 @@ async function promptStacks(opts: {
   const response = await prompts({
     type: "select",
     name: "value",
-    message: `Pick a source of truth for stacks`,
+    message: `Rebase branches or regerate stacks metadata?`,
     choices: ["rebase", "regen"].map((r) => {
       return {
         title:
           r === "rebase"
-            ? `Graphite stacks (${chalk.green("common choice")})\n` +
+            ? `rebase branches, using Graphite stacks as truth (${chalk.green(
+                "common choice"
+              )})\n` +
               opts.metaStack
                 .toString()
                 .split("\n")
                 .map((l) => "    " + l)
                 .join("\n") +
               "\n"
-            : `Git commit tree\n` +
+            : `regen stack metadata, using Git commit tree as truth\n` +
               opts.gitStack
                 .toString()
                 .split("\n")
