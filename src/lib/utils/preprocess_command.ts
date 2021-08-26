29c9b82a4fdbe44dd87ffb326047f4a8e019cc71
Greg Foster <gregorymfoster@users.noreply.github.com>
2021-08-26 09:44:31 -0400

    fix(upstack): two letter alias (#279)

--- src/lib/utils/preprocess_command.ts
+++ src/lib/utils/preprocess_command.ts
@@ -1,5 +1,9 @@
 function splitShortcuts(command: string): string[] {
-  if (typeof command === "string" && command.length == 2) {
+  if (
+    typeof command === "string" &&
+    command.length == 2 &&
+    !["ds", "us"].includes(command) // block list two letter noun aliases
+  ) {
     return [command[0], command[1]];
   } else if (
     typeof command === "string" &&
