a6925704f99b75e55c86b98ff7aa30b5c0ca0b69
Greg Foster <gregorymfoster@users.noreply.github.com>
2021-08-30 14:29:02 -0400

    feat(repo): add alias r (#296)
    * refactor(sync): no change in functionality
    
    * fix(sync): unzit test and fix
    
    * util(preconditions): create auth precondition
    
    * feat(repo): add alias r

--- src/commands/repo.ts
+++ src/commands/repo.ts
@@ -1,5 +1,6 @@
 import { Argv } from "yargs";
 
+export const aliases = ["r"];
 export const command = "repo <command>";
 export const desc =
   "Read or write Graphite's configuration settings for the current repo. Run `gt repo --help` to learn more.";
