0d918d852cd68031f0734325fe96eb59a25f2553
Neha Srivastava <neha@screenplay.dev>
2021-09-24 15:35:39 +0530

    refactor(prev-bottom): fix message

--- src/commands/branch-commands/bottom.ts
+++ src/commands/branch-commands/bottom.ts
@@ -18,7 +18,7 @@ type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 export const command = "bottom";
 export const aliases = ["b"];
 export const description =
-    "If you're in a stack: Branch A → Branch B → Branch C (you are here), checkout the branch at the bottom of the stack (Branch A). If there are multiple parent branches in the stack, `gt bottom` will prompt you to choose which branch to checkout.";
+    "If you're in a stack: Branch A → Branch B → Branch C (you are here), checkout the branch at the bottom of the stack (Branch A).";
 export const builder = args;
 export const handler = async (argv: argsT): Promise<void> => {
     return profile(argv, async () => {
