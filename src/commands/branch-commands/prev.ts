6b1a3a03b80621cfeb7cf0c3c7a692ea47b8ca51
Neha Srivastava <neha@screenplay.dev>
2021-09-24 15:35:39 +0530

    refactor(prev-bottom): fix message

--- src/commands/branch-commands/prev.ts
+++ src/commands/branch-commands/prev.ts
@@ -18,7 +18,7 @@ type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 export const command = "prev [steps]";
 export const aliases = ["p"];
 export const description =
-  "If you're in a stack: Branch A → Branch B (you are here) → Branch C, checkout the branch directly downstack (Branch A). If there are multiple parent branches in the stack, `gt prev` will prompt you to choose which branch to checkout.  Pass the `steps` arg to checkout the branch `[steps]` levels below in the stack.";
+  "If you're in a stack: Branch A → Branch B (you are here) → Branch C, checkout the branch directly downstack (Branch A). Pass the `steps` arg to checkout the branch `[steps]` levels below in the stack.";
 export const builder = args;
 export const handler = async (argv: argsT): Promise<void> => {
   return profile(argv, async () => {
