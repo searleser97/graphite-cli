0bcada977b1cd9eb6649d01fb223325be14ffce7
Neha Srivastava <neha@screenplay.dev>
2021-09-29 14:17:28 +0530

    fix(branch top): better messaging

--- src/commands/branch-commands/top.ts
+++ src/commands/branch-commands/top.ts
@@ -18,7 +18,7 @@ type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 export const command = "top";
 export const aliases = ["t"];
 export const description =
-    "If you're in a stack: Branch A → Branch B (you are here) → Branch C → Branch D , checkout the branch at the top of the stack (Branch D). If there are multiple parent branches in the stack, `gt top` will prompt you to choose which branch to checkout.";
+    "If you're in a stack: Branch A → Branch B (you are here) → Branch C → Branch D , checkout the branch at the top of the stack (Branch D). If there are multiple parent branches in the stack, `gt branch top` will prompt you to choose which branch to checkout.";
 
 export const handler = async (argv: argsT): Promise<void> => {
     return profile(argv, async () => {
