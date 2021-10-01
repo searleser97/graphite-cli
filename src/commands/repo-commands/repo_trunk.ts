cf8f38ef6a6f7564d27c40cc9596f1435366d10b
Nicholas Yan <nicholasyan11@gmail.com>
2021-10-01 15:14:33 -0400

    [Repo Config] Add gt repo trunk --set (#371)

--- src/commands/repo-commands/repo_trunk.ts
+++ src/commands/repo-commands/repo_trunk.ts
@@ -1,9 +1,20 @@
 import chalk from "chalk";
 import yargs from "yargs";
+import { repoConfig } from "../../lib/config";
+import { branchExistsPrecondition } from "../../lib/preconditions";
 import { profile } from "../../lib/telemetry";
 import { getTrunk } from "../../lib/utils";
 
-const args = {} as const;
+const args = {
+  set: {
+    demandOption: false,
+    default: false,
+    type: "string",
+    alias: "s",
+    describe:
+      "Override the value of the repo's trunk branch in the Graphite config.",
+  },
+} as const;
 
 export const command = "trunk";
 export const description =
@@ -13,6 +24,11 @@ export const builder = args;
 type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 export const handler = async (argv: argsT): Promise<void> => {
   return profile(argv, async () => {
-    console.log(`Infered trunk branch = (${chalk.green(getTrunk())})`);
+    if (argv.set) {
+      branchExistsPrecondition(argv.set);
+      repoConfig.setTrunk(argv.set);
+    } else {
+      console.log(`(${chalk.green(getTrunk())})`);
+    }
   });
 };
