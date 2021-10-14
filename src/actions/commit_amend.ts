05e03e5a709a1811f699fc33c3e909be719c958a
Nicholas Yan <nicholasyan11@gmail.com>
2021-10-14 09:09:13 -0400

    Fix commit amend merge conflicts (#404)

--- src/actions/commit_amend.ts
+++ src/actions/commit_amend.ts
@@ -2,6 +2,7 @@ import { execStateConfig } from "../lib/config";
 import { ExitFailedError } from "../lib/errors";
 import { uncommittedChangesPrecondition } from "../lib/preconditions";
 import { gpExecSync, logWarn } from "../lib/utils";
+import Branch from "../wrapper-classes/branch";
 import { fixAction } from "./fix";
 
 export async function commitAmendAction(opts: {
@@ -18,6 +19,14 @@ export async function commitAmendAction(opts: {
         throw new ExitFailedError("Failed to add changes. Aborting...", err);
       }
     );
+  }
+
+  // If we're checked out on a branch, we're going to perform a stack fix later.
+  // In order to allow the stack fix to cut out the old commit, we need to set
+  // the prev ref here.
+  const currentBranch = Branch.getCurrentBranch();
+  if (currentBranch !== null) {
+    currentBranch.setMetaPrevRef(currentBranch.getCurrentRef());
   }
 
   gpExecSync(
